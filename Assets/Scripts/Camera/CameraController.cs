da1d505cc6abc6a07817eea249b38cb95d79db9a
Enzo Tiezzi <enzo_tiezzi@hotmail.com>
2019-02-10 20:58:15 -0200

    testing some scripts

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -7,6 +7,14 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private Transform _follow;
 
+    [SerializeField]
+    private float speedH = 2.0f;
+    [SerializeField]
+    private float speedV = 2.0f;
+
+    private float _yaw;
+    private float _pitch;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +24,12 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(_follow != null)
+        _yaw += speedH * Input.GetAxis("Mouse X");
+        _pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        //transform.eulerAngles = new Vector3(_pitch, _yaw, 0.0f);
+
+        if (_follow != null)
         {
             transform.position = new Vector3(_follow.position.x, 4, _follow.position.z - 10);
         }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,32 +5,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float _speed;
+    private float _speed = 10.0f;
+
+    [SerializeField]
+    private float _rotateSpeed = 0.1f;
+
+    [SerializeField]
+    private float _jumpSpeed = 8.0f;
+
+    [SerializeField]
+    private float _gravity = 20.0f;
+
+    private Vector3 moveDirectrion;
+    private CharacterController _characterController;
+
+    void Awake()
+    {
+        moveDirectrion = Vector3.zero;
+        _characterController = GetComponent<CharacterController>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
     
     void FixedUpdate()
     {
-        var front = Input.GetButton("Front");
-        var back = Input.GetButton("Back");
-        var right = Input.GetButton("Right");
-        var left = Input.GetButton("Left");
+        if(_characterController.isGrounded)
+        {
+            moveDirectrion = new Vector3(0, 0, Input.GetAxis("Vertical"));
+            moveDirectrion = transform.TransformDirection(moveDirectrion);
+            moveDirectrion *= _speed;
 
-        if (front)
-            transform.position += Vector3.forward * _speed * Time.deltaTime;
+            if (Input.GetButton("Jump"))
+                moveDirectrion.y = _jumpSpeed;
+        }
 
-        if (back)
-            transform.position += Vector3.back * _speed * Time.deltaTime;
+        moveDirectrion.y -= _gravity * Time.deltaTime;
+        _characterController.Move(moveDirectrion * Time.deltaTime);
 
-        if (right)
-            transform.position += Vector3.right * _speed * Time.deltaTime;
-
-        if (left)
-            transform.position += Vector3.left * _speed * Time.deltaTime;
+        //transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+        if (Input.GetAxis("Mouse X") < 0)
+            transform.Rotate(Vector3.up * -_speed);
+        if (Input.GetAxis("Mouse X") > 0)
+            transform.Rotate(Vector3.up * _speed);
     }
 }
