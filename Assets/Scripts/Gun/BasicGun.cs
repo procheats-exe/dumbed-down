7811db1abd14745431414f0a782ef96b7c82012e
Enzo Tiezzi <enzo_tiezzi@hotmail.com>
2019-02-16 11:05:33 -0200

    mag code

--- Assets/Scripts/Gun/BasicGun.cs
+++ Assets/Scripts/Gun/BasicGun.cs
@@ -14,9 +14,24 @@ public class BasicGun : MonoBehaviour
     private float _fireRate = 15f;
 
     [SerializeField]
+    private int _totalAmmo = 90;
+
+    [SerializeField]
+    private int _magAmmo = 30;
+
+    private int _currentTotalAmmo;
+    private int _currentMagAmmo;
+
+    [SerializeField]
     private Camera _camera;
 
     private float nextTimeToFire = 0f;
+
+    void Awake()
+    {
+        _currentTotalAmmo = _totalAmmo;
+        _currentMagAmmo = _magAmmo;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -24,22 +39,45 @@ public class BasicGun : MonoBehaviour
         if (Input.GetButton("Fire1") && Time.time > nextTimeToFire)
         {
             nextTimeToFire = Time.time + 1f / _fireRate;
-            Shoot();
+            if (_currentMagAmmo > 0)
+                Shoot();
+            else
+                Reload();
         }
     }
 
     void Shoot()
     {
+        _currentMagAmmo -= 1;
         RaycastHit hit;
 
         if (Physics.Raycast(_camera.transform.position, _camera.transform.forward, out hit, _range))
         {
-            Debug.Log(hit.transform.name);
             var target = hit.transform.GetComponent<Target>();
 
             if (target != null)
             {
                 target.TakeDamange(_damage);
+            }
+        }
+    }
+
+    void Reload()
+    {
+        Debug.Log("Reloading");
+        if(_currentMagAmmo < _magAmmo && _currentTotalAmmo > 0)
+        {
+            var totalAmmo = _currentMagAmmo + _currentTotalAmmo;
+
+            if (totalAmmo > _magAmmo)
+            {
+                _currentMagAmmo = _magAmmo;
+                _currentTotalAmmo = totalAmmo - _currentMagAmmo;
+            }
+            else
+            {
+                _currentMagAmmo = totalAmmo;
+                _currentTotalAmmo -= _currentMagAmmo;
             }
         }
     }
